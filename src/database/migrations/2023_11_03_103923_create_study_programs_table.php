bc69c1a02ea86dd917495e7ef3df3a34a22d59f9
pali33311 <pavol.gajdos@student.ukf.sk>
2023-11-08 19:57:37 +0100

    Add Database Seeder

--- src/database/migrations/2023_11_03_103923_create_study_programs_table.php
+++ src/database/migrations/2023_11_03_103923_create_study_programs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('study_programs', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 45);
+            $table->string('name', 150);
             $table->timestamps();
         });
     }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\StudyProgram;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,15 +15,65 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $roleNames = [
+            "Admin",
+            "Student",
+            "PPP",
+            "Veduci pracoviska",
+            "Zástupca"
+        ];
+
+        foreach ($roleNames as $roleName) {
+            $role = new Role();
+            $role->role = $roleName;
+            $role->save();
+        }
+
+        $studyprograms = [
+            'Aplikovaná ekológia a environmentalistika',
+            'Aplikovaná informatika',
+            'Biológia',
+            'Fyzika',
+            'Geografia v regionálnom rozvoji',
+            'Matematicko-štatistické a informačné metódy v ekonómii a finančníctve',
+            'Učiteľstvo biológie v kombinácii',
+            'Učiteľstvo chémie v kombinácii',
+            'Učiteľstvo ekológie v kombinácii',
+            'Učiteľstvo fyziky v kombinácii',
+            'Učiteľstvo geografie v kombinácii',
+            'Učiteľstvo informatiky v kombinácii',
+            'Učiteľstvo matematiky v kombinácii',
+            'Učiteľstvo odborných ekonomických predmetov v kombinácii',
+        ];
+
+        foreach ($studyprograms as $studyprogram) {
+            $study_program = new StudyProgram();
+            $study_program->name = $studyprogram;
+            $study_program->save();
+        }
+
+
         $user = new User();
-            $user->firstname = "meno";
-            $user->lastname = "meno";
-            $user->email = "eamil7@gmail.com";
-            $user->tel = "9999999";
-            $user->year = "2019";
-            $user->role_id = Role::where('id', 1)->first()->id;
-            $user->password = bcrypt("adminadmin");
-            $user->save();
+        $user->firstname = "meno";
+        $user->lastname = "admin";
+        $user->email = "admin@admin.com";
+        $user->tel = "9999999";
+        $user->year = "2023";
+        $user->role_id = "1";
+        $user->study_program_id = "1";
+        $user->password = bcrypt("adminadmin");
+        $user->save();
+
+        $user = new User();
+        $user->firstname = "meno";
+        $user->lastname = "student";
+        $user->email = "student@student.com";
+        $user->tel = "888888";
+        $user->year = "2022";
+        $user->role_id = "2";
+        $user->study_program_id = "1";
+        $user->password = bcrypt("adminadmin");
+        $user->save();
             
         // \App\Models\User::factory(10)->create();
 
