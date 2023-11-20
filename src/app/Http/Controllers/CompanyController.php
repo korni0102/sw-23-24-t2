21d99bce5cc533bde10dabcd694438595e786b2e
maximlepjak <maxim.lepjak@gmail.com>
2023-11-21 00:25:32 +0100

    Feature-addCompany

--- src/app/Http/Controllers/CompanyController.php
+++ src/app/Http/Controllers/CompanyController.php
@@ -9,9 +9,33 @@ class CompanyController extends Controller
 {
     public function showCompanies() {     
         $companies = Company::all();
-        return view('admin_view_companies', ['companies' => $companies]); 
+        return view('admin_view_companies', ['companies' => $companies]);  
+    }
+    
+    public function addCompany()
+    {
+        return view('addCompany');
+    }
+
+    // Method to handle the form submission
+    public function saveCompany(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:45',
+            'address' => 'required|string|max:45',
+        ]);
+
+        $companies = Company::create([
+            'name' => $validatedData['name'],
+            'address' => $validatedData['address'],
+        ]);
+
+        return redirect()->route('companies')->with("success", 'Pridanie prebehlo úspešne!');
     }
 }
+     
+
+    
 
 
     
--- src/routes/web.php
+++ src/routes/web.php
@@ -36,3 +36,8 @@ Route::get('/jobs', [JobController::class, 'showJobs'])->name('jobs');
 //rolerequest
 Route::get('/request/modify', [AdminController::class, 'modifyRoleRequest'])->name('admin.modifyRoleRequest');
 Route::post('/request/changeStatus/{role_request_id}/{role_request_status}', [AdminController::class, 'asd'])->name('admin.changeRequestStatus');
+
+//addCompany
+Route::get('/addCompany', [CompanyController::class, 'addCompany'])->name('addCompany');
+Route::post('/saveCompany', [CompanyController::class, 'saveCompany'])->name('saveCompany');
+
