598a8b67ea18b3af9aa2832d161710f598b8aaa7
korni0102 <kornel.takacs@student.ukf.sk>
2023-11-29 07:47:36 +0100

    Feature - Fix

--- src/app/Http/Controllers/JobController.php
+++ src/app/Http/Controllers/JobController.php
@@ -7,37 +7,34 @@ use App\Models\Contract;
 
 class JobController extends Controller
 {
-    // Existing function to show all jobs
-    public function showJobs() {     
-        $jobs = Job::with('contract')->get(); // Ensure to eager load the contract relationship
-        return view('jobsView', ['jobs' => $jobs]); 
+
+    public function showJobs() {
+        //$jobs = Job::all();
+
+        $jobs = Job::with('contract')->get();
+        return view('jobsView', ['jobs' => $jobs]);
     }
 
-    // Function to show part-time jobs
     public function showPartTimeJobs() {
         $partTimeJobs = Job::where('job_type', 'parttime')->get();
         return view('jobs_type.parttime', ['jobs' => $partTimeJobs]);
     }
 
-    // Function to show paid jobs
     public function showPaidJobs() {
         $paidJobs = Job::where('job_type', 'paid')->get();
         return view('jobs_type.paid', ['jobs' => $paidJobs]);
     }
 
-    // Function to show unpaid jobs
     public function showUnpaidJobs() {
         $unpaidJobs = Job::where('job_type', 'unpaid')->get();
         return view('jobs_type.unpaid', ['jobs' => $unpaidJobs]);
     }
 
-    // Function to show full-time jobs
     public function showFullTimeJobs() {
         $fullTimeJobs = Job::where('job_type', 'fulltime')->get();
         return view('jobs_type.fulltime', ['jobs' => $fullTimeJobs]);
     }
 
-    // Function to show freelancer jobs
     public function showFreelancerJobs() {
         $freelancerJobs = Job::where('job_type', 'freelancer')->get();
         return view('jobs_type.freelancer', ['jobs' => $freelancerJobs]);
